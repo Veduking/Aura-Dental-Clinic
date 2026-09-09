0b2166c1de67f25620b505c56693038fb9a0a90b
Veduking <vedusimons12@gmail.com>
2026-09-09 19:43:44 +0530

    Set output configuration to 'export' in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     // Images are local public assets, so avoid a remote optimizer dependency.
     unoptimized: true,
